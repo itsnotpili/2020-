20a2816724d2886c545770300bec608121072f37
p i l i ♥ <53492030+itsnotpili@users.noreply.github.com>
2020-02-21 18:11:37 -0300

    Update ejercicio 1 tp 1.py

--- ejercicio 1 tp 1.py
+++ ejercicio 1 tp 1.py
@@ -1,32 +1,25 @@
-def MayorEstricto(a,b,c):
-    mayor=-1
+def estricto(a,b,c):
+    mayor=c
     if a>b:
-        if a != c:
-            if a>c:
-                mayor=a
-            else:
-                mayor=c
-    elif b>a:
-        if b!=c:
-            if b>c:
-                mayor=b
-            else:
-                mayor=c
+        if a!=b: 
+            if a!=c:
+                if a>c:
+                    mayor=a
+    elif b!=c:
+        if b>c:
+            mayor=b
+    else:
+        mayor="No existe mayor estricto"
     return mayor
-num=int(input('Ingrese un numero: '))
-while num<=0:
-    print('El numero debe ser positivo')
-    num=int(input('Ingrese un numero: '))
-num1=int(input('Ingrese un numero: '))
-while num1<=0:
-    print('El numero debe ser positivo')
-    num1=int(input('Ingrese un numero: '))
-num2=int(input('Ingrese un numero: '))
-while num2<=0:
-    print('El numero debe ser positivo')
-    num2=int(input('Ingrese un numero: '))
-funcion=MayorEstricto(num,num1,num2)
-if funcion==-1:
-    print('No existe el mayor estricto')
-else:
-    print(funcion)               
+
+num1=int(input("num1: "))
+while (num1) <= 0:        
+    num1=int(input("num1: "))
+num2=int(input("num2: "))
+while (num2) <= 0:        
+    num2=int(input("num2: "))
+num3=int(input("num3: "))
+while (num3) <= 0:        
+    num3=int(input("num3: "))
+
+print(estricto(num1,num2,num3))
